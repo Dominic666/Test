d65fb0de0e418cd05d89615bd69bc575b9f2eab8
Dominic666 <dominic666@aol.com>
2015-08-24 14:09:37 +0100

    Original

---
+++ RealMotion Create Installer/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace RealMotion_Create_Installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SerDir = new System.Windows.Forms.Button();
+            this.tb_files = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.vb_version = new System.Windows.Forms.TextBox();
+            this.bt_create = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lb_version = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_MainDir = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_server = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SerDir
+            // 
+            this.SerDir.Location = new System.Drawing.Point(499, 39);
+            this.SerDir.Name = "SerDir";
+            this.SerDir.Size = new System.Drawing.Size(82, 23);
+            this.SerDir.TabIndex = 0;
+            this.SerDir.Text = "Set Directory";
+            this.SerDir.UseVisualStyleBackColor = true;
+            this.SerDir.Click += new System.EventHandler(this.SerDir_Click);
+            // 
+            // tb_files
+            // 
+            this.tb_files.Location = new System.Drawing.Point(93, 41);
+            this.tb_files.Name = "tb_files";
+            this.tb_files.Size = new System.Drawing.Size(400, 20);
+            this.tb_files.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Files Directory";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Version No:";
+            // 
+            // vb_version
+            // 
+            this.vb_version.Location = new System.Drawing.Point(92, 123);
+            this.vb_version.Name = "vb_version";
+            this.vb_version.Size = new System.Drawing.Size(114, 20);
+            this.vb_version.TabIndex = 6;
+            // 
+            // bt_create
+            // 
+            this.bt_create.Location = new System.Drawing.Point(474, 149);
+            this.bt_create.Name = "bt_create";
+            this.bt_create.Size = new System.Drawing.Size(106, 23);
+            this.bt_create.TabIndex = 8;
+            this.bt_create.Text = "Create and Upload";
+            this.bt_create.UseVisualStyleBackColor = true;
+            this.bt_create.Click += new System.EventHandler(this.bt_create_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Current Version:";
+            // 
+            // lb_version
+            // 
+            this.lb_version.AutoSize = true;
+            this.lb_version.Location = new System.Drawing.Point(99, 175);
+            this.lb_version.Name = "lb_version";
+            this.lb_version.Size = new System.Drawing.Size(31, 13);
+            this.lb_version.TabIndex = 10;
+            this.lb_version.Text = "0.0.0";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 149);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(453, 23);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Main Game";
+            // 
+            // tb_MainDir
+            // 
+            this.tb_MainDir.Location = new System.Drawing.Point(93, 5);
+            this.tb_MainDir.Name = "tb_MainDir";
+            this.tb_MainDir.Size = new System.Drawing.Size(400, 20);
+            this.tb_MainDir.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(499, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Set Directory";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Server Directory";
+            // 
+            // tb_server
+            // 
+            this.tb_server.Location = new System.Drawing.Point(102, 80);
+            this.tb_server.Name = "tb_server";
+            this.tb_server.Size = new System.Drawing.Size(391, 20);
+            this.tb_server.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(499, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Set Directory";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 201);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_server);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tb_MainDir);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lb_version);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.bt_create);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.vb_version);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_files);
+            this.Controls.Add(this.SerDir);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "RealMotion Create Installer App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button SerDir;
+        private System.Windows.Forms.TextBox tb_files;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox vb_version;
+        private System.Windows.Forms.Button bt_create;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lb_version;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_MainDir;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_server;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RealMotion Create Installer/Form1.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
+using System.Net;
+
+
+namespace RealMotion_Create_Installer
+{
+    public partial class Form1 : Form
+    {
+        private string Source;
+        private string Destination;
+        MySqlConnection conn;
+        private string myConnectionString = "Server=box352.bluehost.com;Database=blazarga_BlazarGames;User ID=blazarga_Dominic;Password=File66677;";
+        private string GetVersion = "SELECT VersionNumber FROM RealMotionInfo ORDER BY ID DESC LIMIT 1";
+        List<String> FilesWithChanges = new List<String>();
+        string DefaultFileLocation = @"C:\Doms\Software\UE4\Builds\RealMotion\WindowsNoEditor";
+        string MasterFilesLocation = @"C:\Doms\Software\UE4\Builds\RealMotion MASTER";
+        string ServerFilesLocation = @"C:\Doms\Software\UE4\RealMotion\Saved\StagedBuilds\WindowsServer";
+        string ServerTargetLocation = @"\\Domio-pc\WindowsServer";
+        float TotalSize = 0;
+        List<String> UpdateList = new List<String>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            tb_files.Text = DefaultFileLocation;
+            tb_MainDir.Text = MasterFilesLocation;
+
+            try
+            {
+                conn = new MySqlConnection(myConnectionString);
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand(GetVersion, conn);
+                lb_version.Text = Convert.ToString(cmd.ExecuteScalar()); ;
+
+                conn.Close();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show("Server is down");
+            }
+
+            
+        }
+
+        private void SerDir_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Source = folderBrowserDialog1.SelectedPath;
+                tb_files.Text = Source;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Destination = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void bt_create_Click(object sender, EventArgs e)
+        {
+            CompareFiles();
+        }
+
+        private void ReadyToUpload()
+        {
+            float TempSize = TotalSize;
+            string sizestring = "";
+            if (TempSize < 10000000)
+            {
+                if (TempSize < 1000)
+                {
+                    sizestring = TempSize + " Bytes";
+                }
+                else
+                {
+                    sizestring = (TempSize / 1024) + " Kbs";
+                }
+            }
+            else
+            {
+                sizestring = ((TempSize / 1024)/1024) + " Kbs";
+            }
+            if (FilesWithChanges.Count > 1)
+            {
+                sizestring = "Ready to upload: Files: " + (FilesWithChanges.Count - 1).ToString() + " Total Size: " + sizestring;
+                DialogResult dialogResult = MessageBox.Show(sizestring, "Upload", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    UpdateSQLAndUpload();
+                }
+            }
+            else
+            {
+                MessageBox.Show("No new files to update");
+            }
+        }
+
+        private float GetTotalSize()
+        {
+            TotalSize = 0;
+            for (int i = 0; i < FilesWithChanges.Count - 1; i++)
+            {
+                FileInfo fi = new FileInfo(FilesWithChanges[i]);
+                TotalSize += fi.Length;
+            }
+            return (float)Math.Round(TotalSize,4);
+        }
+
+        private void UpdateSQLAndUpload()
+        {
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void SetSQL()
+        {
+            string SetVersion = "INSERT INTO RealMotionInfo (VersionNumber, Date) VALUES ('" + vb_version.Text + "','" + DateTime.Now.ToString("d/M/yyyy") + "')";
+           
+            conn = new MySqlConnection(myConnectionString);
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand(SetVersion, conn);
+         //   lb_version.Text = Convert.ToString(cmd.ExecuteScalar());
+            string newversion = Convert.ToString(cmd.ExecuteScalar());
+            lb_version.BeginInvoke((Action)delegate() { lb_version.Text = newversion; });
+            conn.Close();
+
+            MessageBox.Show("Upload Completed");
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string ftplocation = "ftp://ftp.blazargames.com/RealMitionVersions/Updates";
+            string file = "/RealMotion" + vb_version.Text.Replace(".", "_") + ".zip";
+            string user = "UE4@blazargames.com";
+            string password = "File6667788";
+            string FileToUpload = file;
+
+
+            string[] filePaths1 = Directory.GetFiles(ServerFilesLocation, "*", SearchOption.AllDirectories);
+
+            for (int i = 0; i < filePaths1.Length-1;i++)
+            {
+                FileInfo file2 = new FileInfo(filePaths1[i]);
+                string f1 = file2.FullName.Replace(ServerFilesLocation, "");
+                FileInfo destFile = new FileInfo(ServerTargetLocation + f1);
+                if (destFile.Exists)
+                {
+                    if (file2.LastWriteTime > destFile.LastWriteTime)
+                    {
+                        file2.CopyTo(destFile.FullName, true);
+                    }
+                }
+                else
+                {
+                    string DestFolder = destFile.Directory.ToString();
+                    if (!Directory.Exists(DestFolder))
+                    {
+                        Directory.CreateDirectory(DestFolder);
+                    }
+                    file2.CopyTo(destFile.FullName, true);
+                }
+            }
+
+            
+
+            FileStream fs = null;
+            Stream rs = null;
+
+            float totalReadBytesCount = 0;
+
+            for (int i = 0; i < FilesWithChanges.Count; i++)
+            {
+                try
+                {
+                    string uploadFileName = Path.GetFileName(FilesWithChanges[i]);
+                    file = FilesWithChanges[i];
+                    string uploadUrl = ftplocation;
+                    fs = new FileStream(Destination + file, FileMode.Open, FileAccess.Read);
+                    string ftpUrl = string.Format("{0}/{1}", uploadUrl, uploadFileName);
+                    FtpWebRequest requestObj = FtpWebRequest.Create(ftpUrl) as FtpWebRequest;
+                    requestObj.Method = WebRequestMethods.Ftp.UploadFile;
+                    requestObj.Credentials = new NetworkCredential(user, password);
+                    rs = requestObj.GetRequestStream();
+
+                    byte[] buffer = new byte[1024];
+                    int read = 0;
+                    
+                    while ((read = fs.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        rs.Write(buffer, 0, read);
+                        totalReadBytesCount += buffer.Length;
+                        double progress = totalReadBytesCount * 100.0 / TotalSize;
+                        if (progress > 100)
+                        {
+                            progress = 100;
+                        }
+                        backgroundWorker1.ReportProgress((int)progress);
+                    }
+                    rs.Flush();
+                }
+                catch (Exception ex)
+                {
+                    
+                    MessageBox.Show("File upload/transfer Failed.\r\nError Message:\r\n" + ex.Message, "Succeeded", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                finally
+                {
+                    if (fs != null)
+                    {
+                        fs.Close();
+                        fs.Dispose();
+                    }
+
+                    if (rs != null)
+                    {
+                        rs.Close();
+                        rs.Dispose();
+                    }
+                }
+            }
+            SetSQL();
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Destination = folderBrowserDialog1.SelectedPath;
+                tb_MainDir.Text = Destination;
+            }
+        }
+
+        private void CompareFiles()
+        {
+
+         string[] filePaths1 = Directory.GetFiles(tb_MainDir.Text,"*", SearchOption.AllDirectories);
+
+         string[] filePaths2 = Directory.GetFiles(tb_files.Text,"*", SearchOption.AllDirectories);
+         FilesWithChanges.Clear();
+
+         for (int i = 0;i < filePaths2.Length; i++)
+         {
+             bool FoundMatch = false;
+             for (int ii = 0; ii < filePaths1.Length; ii++)
+             {
+                 if (filePaths2[i].Replace(tb_files.Text, "") == filePaths1[ii].Replace(tb_MainDir.Text, ""))
+                 {
+                     FoundMatch = true;
+                     FileInfo File1 = new FileInfo(filePaths1[ii]);
+                     FileInfo File2 = new FileInfo(filePaths2[i]);
+
+                     
+                     if (File1.Length != File2.Length)
+                     {
+                         FilesWithChanges.Add(filePaths2[i]);
+                     }
+                     break;
+                 }
+             }
+             if (FoundMatch == false)
+             {
+                 FilesWithChanges.Add(filePaths2[i]);
+             }
+         }
+         GetNeededFilesList();
+         
+        }
+
+        private void GetNeededFilesList()
+        {
+            string _address = "blazargames.com";
+            string _userName = "UE4@blazargames.com";
+            string _password = "File6667788";
+            string AppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\RealMotion";
+            string _localPath = AppData + "temp\\";
+
+        string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments);
+            var directory = new DirectoryInfo(documentsPath);
+            //System.IO.StreamWriter file = new System.IO.StreamWriter(directory.Parent.FullName + "\\UpdateList.txt");
+
+            string fileName = "RealMitionVersions/Updates/UpdateList.txt";
+
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://" + _address + "/" + fileName);
+            request.Credentials = new NetworkCredential(_userName, _password);
+            request.Method = WebRequestMethods.Ftp.GetFileSize;
+            request.Proxy = null;
+
+            long fileSize; // this is the key for ReportProgress
+            using (WebResponse resp = request.GetResponse())
+                fileSize = resp.ContentLength;
+
+            request = (FtpWebRequest)WebRequest.Create("ftp://" + _address + "/" + fileName);
+            request.Credentials = new NetworkCredential(_userName, _password);
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+            //System.IO.Directory.CreateDirectory(MainDrive + "temp");
+            using (FtpWebResponse responseFileDownload = (FtpWebResponse)request.GetResponse())
+            using (Stream responseStream = responseFileDownload.GetResponseStream())
+            using (FileStream writeStream = new FileStream(directory.Parent.FullName + "\\UpdateList.txt", FileMode.Create))
+            {
+
+                int Length = 2048;
+                Byte[] buffer = new Byte[Length];
+                int bytesRead = responseStream.Read(buffer, 0, Length);
+                int bytes = 0;
+
+                while (bytesRead > 0)
+                {
+                    writeStream.Write(buffer, 0, bytesRead);
+                    bytesRead = responseStream.Read(buffer, 0, Length);
+                    bytes += bytesRead;// don't forget to increment bytesRead !
+                    int totalSize = (int)(fileSize) / 1000; // Kbytes
+                    //backgroundWorker2.ReportProgress(100);
+                }
+            }
+
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(directory.Parent.FullName + "\\UpdateList.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line == "") continue;
+                bool NewFile = false;
+                string[] temp = line.Split(':');
+                string fileN = temp[0] + temp[1];
+                fileN = Path.GetFileName(fileN);
+                foreach (string row in FilesWithChanges)
+                {
+                    string Name = Path.GetFileName(row);
+                    if (Name == fileN)
+                    {
+                        NewFile = true;
+                        FileInfo F2 = new FileInfo(row);
+                        if (temp[2] == F2.Length.ToString())
+                        {
+                            NewFile = false;
+                            FilesWithChanges.Remove(row);
+                            break;
+                        }
+                    }
+                }
+                if (NewFile == false)
+                {
+                    UpdateList.Add(line);
+                }
+                
+            }
+
+            file.Close();
+
+
+            CreateChangesFile();
+        }
+
+        private void CreateChangesFile()
+        {
+            if (FilesWithChanges.Count > 0)
+            { 
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments);
+            var directory = new DirectoryInfo(documentsPath);
+            System.IO.StreamWriter file = new System.IO.StreamWriter(directory.Parent.FullName + "\\UpdateList.txt");
+
+                foreach(string s in UpdateList)
+                {
+                    file.WriteLine(s);
+                }
+
+            foreach (string s in FilesWithChanges)
+                {
+                    FileInfo f = new FileInfo(s);
+                    file.WriteLine(s + ":" + f.Length.ToString());
+                }
+
+           
+            file.Close();
+            FilesWithChanges.Add(directory.Parent.FullName + "\\UpdateList.txt");
+            TotalSize = GetTotalSize();
+            ReadyToUpload();
+            }
+            else
+            {
+                MessageBox.Show("No changes found");
+                return;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tb_server.Text = ServerFilesLocation;
+        }
+    }
+}
